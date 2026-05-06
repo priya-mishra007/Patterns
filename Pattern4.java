d3bd535412fa35f4a3711f4364485cf6104623eb
priya-mishra007 <shashimaa87@gmail.com>
2026-05-06 16:26:57 +0530

    Diamond Number Pattern
    This Java program prints a number pattern in two parts:
    
    Upper Part: Numbers increase row by row
    Lower Part: Numbers decrease row by row
    
    Each row prints the current row number multiple times.

---
+++ Pattern4.java
@@ -0,0 +1,18 @@
+public class Pattern {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 3; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        for(int i = 2; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
